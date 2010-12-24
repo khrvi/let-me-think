8c1069bcca6c15b5d808d03f3181c1e34b7fd946
Viacheslav Khrenov <khrviach@gmail.com>
2010-12-24 23:47:19 +0200

    attempt to use dynamic questions

---
+++ lib/questionnaire.rb
@@ -0,0 +1,58 @@
+require 'humane_integer'
+
+ # Simple model to hold sets of questions and answers.
+class Questionnaire
+  
+  def initialize
+    file = File.join(Rails.root, "config/questionnaire.yml")
+    config
+  rescue
+    raise "Config file is not found"
+  end
+  
+  def config
+    @config ||= YAML::load(File.open(file))
+  end
+
+  # Attempt to answer a captcha, returns true if the answer is correct.
+  def self.attempt?(string, answer)
+    puts "!!!!#{answer}!!"
+    string = string.strip.downcase
+    if answer_is_integer?
+      return string == answer || string == HumaneInteger.new(answer.to_i).to_english
+    else
+      return string == answer.downcase
+    end
+  end
+
+  def self.find_random_question
+    find_random
+  end
+
+  private
+
+  def self.find_random
+    #find(:first, :conditions => ['locale = ?', I18n.locale.to_s], :order => random_function)
+    questions = config['questions'][I18n.locale.to_s]
+    q = questions[rand(questions.size)]
+    interval = (config['interval'] || 50).to_i
+    first_number = rand(interval)
+    last_number = rand(interval)
+    result = first_number + last_number
+    q.gsub('$1', first_number.to_english)
+    q.gsub('$2', first_number.to_english)
+    [q, result]
+  end
+
+  # def self.find_specific_or_fallback(id)
+  #   find(id)
+  # rescue ActiveRecord::RecordNotFound
+  #   find_random
+  # end
+
+  def answer_is_integer?
+    int_answer = answer.to_i
+    (int_answer != 0) || (int_answer == 0 && answer == "0")
+  end
+
+end
