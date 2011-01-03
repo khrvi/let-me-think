0fd13e275ae7aff764c8d210dadddd98ed9c73c9
Viacheslav Khrenov <khrviach@gmail.com>
2011-01-03 12:54:54 +0200

    changed several options for markup

--- lib/smart_capcha_helper.rb
+++ lib/smart_capcha_helper.rb
@@ -3,9 +3,9 @@ module LetMeThink
     def smart_capcha
       result = ''
       result << "<span id='smart_capcha'>"
-      result << "<label for='captcha_answer'><label for='captcha_answer'>"
+      result << "<label for='captcha_answer'>"
       result << "Spam protection: #{@captcha}</label>"
-      result << text_field_tag(:captcha_answer, params[:captcha_answer], :size => 10, :maxlength => 40)
+      result << text_field_tag(:captcha_answer, params[:captcha_answer], :size => 30)
       result << "</span>"
       result.html_safe
     end
